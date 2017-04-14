1ce5b0f6a8f9bb2c1f40f68a8346595e1c7559ae
Andi Andreas <nexuister@gmail.com>
2017-04-14 14:41:10 -0400

    Repo downloading & unzipping functionality

--- js/index.js
+++ js/index.js
@@ -2,43 +2,94 @@ const application = require("electron").remote.app;
 const {net} = require("electron").remote;
 const path = require("path");
 const fs = require("fs");
+const Zip = require("adm-zip");
+//const harp = require("harp");
 
-const repoURL = "https://github.com/UniversityHigh/universityhigh.github.io/archive/master.zip";
+const repoURL = "https://github.com/UniversityHigh/universityhigh.github.io/archive/master.zip"; // Repo can be downloaded from here
+const appPath = application.getPath("userData"); // All app files go here
+const repoPath = path.join(appPath, "universityhigh.github.io-master"); // Repo should be here
 
 new Vue({
 	el: "#app",
 	data: {
 		authenticated: false,
 		topLabel: "Searching for repo...",
-		bottomLabel: "",
-		repoFound: false
+		bottomLabel: "Server is offline.",
+		repoFound: false,
+		logText: ""
 	},
-	created: function() {
-		let repoPath = path.join(application.getPath("userData", "repo"), "repo");
-		if (fs.existsSync(repoPath)) {
-			this.topLabel = "Found repo: " + repoPath
-			repoFound = true
-		}
-		else {
-			this.topLabel = "Couldn't find repo. Need to download"
-			repoFound = false
-		}
+	mounted: function() {
+		this.checkForRepo();
 	},
 	methods: {
-		downloadRepo: () => {
-			console.log("download");
+		checkForRepo: function() {
+			if (fs.existsSync(repoPath)) {
+				this.log("Found");
+				this.topLabel = "Found repo: " + repoPath;
+				this.repoFound = true;
+			}
+			else {
+				this.topLabel = "Couldn't find repo. Need to download.";
+				this.repoFound = false;
+			}
+		},
+		downloadRepo: function() {
 			let request = new net.request(repoURL);
+			let currentBytes = 0;
+			let totalBytes = 0;
+			this.log("Sending download request...");
 			request.on("response", (res) => {
-				let stream = fs.createWriteStream("/Users/Andi/Downloads/repo.zip");
-				res.pipe(stream);
+				if (!("content-length" in res.headers)) {
+					this.log("Request didn't contain a content-length???");
+					return;
+				}
+				totalBytes = res.headers["content-length"][0]; // Might not always exist
+				res.on("data", (data) => {
+					currentBytes += data.length;
+					fs.appendFileSync(`${repoPath}.zip`, data);
+					this.topLabel = `Downloading repo zip file (${currentBytes} bytes / ${totalBytes} bytes)`;
+				})
 			})
 			request.on("close", () => {
-				console.log("closed");
+				this.log("Request closed");
+				if (currentBytes == totalBytes) {
+					this.log("Download successful, unzipping repo...");
+					let zip = new Zip(`${repoPath}.zip`);
+					zip.extractAllTo(appPath);
+					this.log("Repo unzipped");
+					fs.unlink(`${repoPath}.zip`, () => {}); // No longer any need for the zip, also this has an empty callback because apparently not passing in a callback is deprecated (?)
+					this.checkForRepo();
+				}
+				else {
+					this.log("Download unsuccessful");
+				}
 			});
 			request.on("error", (err) => {
-				console.log("err");
+				this.log(`Request errored: ${err}`);
 			});
 			request.end();
+		},
+		deleteRepo: function() {
+			this.topLabel = "Deleting repo...";
+			fs.rmdir(repoPath, (err) => {
+				if (err) {
+					this.log(`Failed to delete: ${err}`);
+				}
+				else {
+					this.log("Deleted repo successfully");
+				}
+				this.checkForRepo();
+			});
+		},
+		startServer: function() {
+			this.log("Starting server...");
+			harp.server(repoPath, function() {
+				this.log("end");
+			});
+		},
+		log: function(msg) {
+			this.logText += msg + "\n";
+			console.log(msg);
 		}
 	}
 });
